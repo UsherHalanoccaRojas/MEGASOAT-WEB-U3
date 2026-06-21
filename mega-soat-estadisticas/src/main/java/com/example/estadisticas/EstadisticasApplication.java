a9a49b6f000a9c1bd7c2f9142120928da9a41a3d
Jhony Vargas <jv2022075754@virtual.upt.pe>
2026-06-21 16:19:32 -0500

    Backend: Clase principal del microservicio de Estadisticas

---
+++ mega-soat-estadisticas/src/main/java/com/example/estadisticas/EstadisticasApplication.java
@@ -0,0 +1,12 @@
+package com.example.estadisticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstadisticasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstadisticasApplication.class, args);
+    }
+}
